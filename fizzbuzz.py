4732243def30e3e1f3b9325b595c974dc52540ae
jaeseong-kang <kg9884@naver.com>
2023-09-18 12:22:45 +0900

    Merge branch buzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,16 +1,11 @@
 def fizzbuzz(n:int):
     for i in range(1,n+1):
-<<<<<<< HEAD
         if i%3==0:
             print("fizz")
+        elif i%5==0:
+            print("buzz")
         else:
             print(i)
 
 if __name__=='__main__':
     fizzbuzz(15)
-=======
-        if i%5==0:
-            print("buzz")
-        else:
-            print(i)
->>>>>>> buzz
