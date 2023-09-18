7b769e2dadc335a3529b1576d5802a8d8dd8ff1a
jaeseong-kang <kg9884@naver.com>
2023-09-18 12:14:20 +0900

    feat: fizzbuzz function update
    fizzbuzz take argument n and 1~n repeat
    if repeat number is multiple number of 3, print fizz else print repeat number

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,9 @@
-print("hello")
+def fizzbuzz(n:int):
+    for i in range(1,n+1):
+        if i%3==0:
+            print("fizz")
+        else:
+            print(i)
+
+if __name__=='__main__':
+    fizzbuzz(15)
